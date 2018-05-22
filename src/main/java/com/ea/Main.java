94b20fdebb2646c9e075b5fa3f9037b146d90773
DevSolu <kameswara.satyavolu@softtek.com>
2018-05-23 00:16:55 +0530

    Update main java file

--- src/main/java/com/ea/Main.java
+++ src/main/java/com/ea/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-   System.out.print("This is my new Window");
+   System.out.print("This is my new Windowi 2018 which i have added:");
 
     }
 }
